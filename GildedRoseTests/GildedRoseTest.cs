19d11b94181bfb90bcfcb432df6b47b9bcbd74e4
foufa foufa <fadoua-yahia@outlook.com>
2023-08-04 15:40:35 +0200

    "Refactoring The ProductQuality Project "

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -20,7 +20,7 @@ namespace GildedRoseTests
         [Fact]
         public void TestGenericProduct_SellInAndQualityReduced()
         {
-            Item item = CreateAndUpdateItem("foo",4,3);
+            Item item = CreateAndUpdateItem(GildedRose.GenericPrdExpName, 4,3);
 
             Assert.Equal(3,item.SellIn);
             Assert.Equal(2, item.Quality);
@@ -28,14 +28,14 @@ namespace GildedRoseTests
         [Fact]
         public void TestGenericPrdOutOfDate_QualityDecreaseTwice()
         {
-            Item item = CreateAndUpdateItem("foo", 0, 5);
+            Item item = CreateAndUpdateItem(GildedRose.GenericPrdExpName, 0, 5);
 
             Assert.Equal(3, item.Quality);
         }
         [Fact]
         public void TestGenericPrd_QualityNotNegative()
         {
-            Item item = CreateAndUpdateItem("foo", 3, 0);
+            Item item = CreateAndUpdateItem(GildedRose.GenericPrdExpName, 3, 0);
 
             Assert.Equal(0, item.Quality);
         }
@@ -52,25 +52,25 @@ namespace GildedRoseTests
         [Fact]
         public void TestAgedBrie_QualityIncrease()
         {
-            Item item = CreateAndUpdateItem("Aged Brie", 3, 4);
+            Item item = CreateAndUpdateItem(GildedRose.AgedBrie, 3, 4);
             Assert.Equal(5, item.Quality);
         }
         [Fact]
         public void TestAgedBrie_QualityNotSuperiorTo50()
         {
-            Item item = CreateAndUpdateItem("Aged Brie", 3, 50);
+            Item item = CreateAndUpdateItem(GildedRose.AgedBrie, 3, 50);
             Assert.Equal(50, item.Quality);
         }
         [Fact]
         public void TestAgedBrieOutOfDate_QualityIncreaseTwice()
         {
-            Item item = CreateAndUpdateItem("Aged Brie", 0, 3);
+            Item item = CreateAndUpdateItem(GildedRose.AgedBrie, 0, 3);
             Assert.Equal(5, item.Quality);
         }
         [Fact]
         public void TestAgedBrieOutOfDate_QualityNotSuperiorTo50()
         {
-            Item item = CreateAndUpdateItem("Aged Brie", 0, 49);
+            Item item = CreateAndUpdateItem(GildedRose.AgedBrie, 0, 49);
             Assert.Equal(50, item.Quality);
         }
         #endregion
@@ -79,13 +79,13 @@ namespace GildedRoseTests
         [Fact]
         public void TestSulfurasPrd_QualityIsConstant()
         {
-            Item item = CreateAndUpdateItem("Sulfuras, Hand of Ragnaros", 5, 80);
+            Item item = CreateAndUpdateItem(GildedRose.Sulfuras, 5, 80);
             Assert.Equal(80, item.Quality);
         }
         [Fact]
         public void TestSulfurasPrdOutOfDate_QualityIsConstant()
         {
-            Item item = CreateAndUpdateItem("Sulfuras, Hand of Ragnaros", 0, 80);
+            Item item = CreateAndUpdateItem(GildedRose.Sulfuras, 0, 80);
             Assert.Equal(80, item.Quality);
         }
         #endregion
@@ -93,43 +93,43 @@ namespace GildedRoseTests
         [Fact]
         public void TestBackstagePrd_QualityIncrease()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 25, 7);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 25, 7);
             Assert.Equal(8, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdSellInIsTenOrLess_QualityIncreaseTwice()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 10, 7);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 10, 7);
             Assert.Equal(9, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdSellInIsFiveOrLess_QualityIncreaseBy3()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 5, 7);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 5, 7);
             Assert.Equal(10, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdOutOfDate_QualityEqualsZero()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 0, 5);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 0, 5);
             Assert.Equal(0, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdSellInMoreThanTen_QualityNotSuperior50()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 40, 49);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 40, 49);
             Assert.Equal(50, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdSellInIsTenOrLess_QualityNotSuperior50()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 10, 49);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 10, 49);
             Assert.Equal(50, item.Quality);
         }
         [Fact]
         public void TestBackstagePrdSellInIsFiveOrLess_QualityNotSuperior50()
         {
-            Item item = CreateAndUpdateItem("Backstage passes to a TAFKAL80ETC concert", 5, 49);
+            Item item = CreateAndUpdateItem(GildedRose.BackStagePasses, 5, 49);
             Assert.Equal(50, item.Quality);
         }
         #endregion
